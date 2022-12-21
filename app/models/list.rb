5aabc4921f3ee380d418e1cafcae0cba91de4754
Claire Deville <clairedeville@gmail.com>
2022-12-21 12:51:53 +0100

    front ok

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,13 @@ class List < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
+  def background_url
+    if photo.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(photo, only_path: true)
+    else
+      ActionController::Base.helpers.image_path("jeremy-yap-J39X2xX_8CQ-unsplash.jpg")
+    end
+  end
 end
